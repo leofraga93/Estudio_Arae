4cd897391e5fbe5428a42e247447ec8844875e8c
leofraga93 <49664647+leofraga93@users.noreply.github.com>
2025-03-31 22:32:25 -0300

    Incluindo campo de páginas, contendo um novo carrossel para indicar os trabalhos feitos com a possibiblidae de navegação entre as imagens de forma estática, difernete do primeiro.

--- script.js
+++ script.js
@@ -22,3 +22,31 @@ imagensDestaque.forEach((img, index) => {
   img.style.marginright = '-20px';
 })
 */
+
+const slides = document.querySelectorAll('#paginas .slide');
+const prevButton = document.querySelector('#prev-button');
+const nextButton = document.querySelector('#next-button');
+
+let currentIndex = 0;
+
+function showSlide(index) {
+  slides.forEach((slideIndex, i) => {
+    if (i === index) {
+      slideIndex.classList.add("active");
+    } else {
+      slideIndex.classList.remove("active");
+    }
+  });
+}
+
+prevButton.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+  showSlide(currentIndex);
+});
+
+nextButton.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide(currentIndex);
+});
+
+showSlide(currentIndex);
